4cf47f71ea8605e81dbdca7ddb2096c8b62d37da
Caleb-Huss <caleb.huss97@gmail.com>
2021-07-22 09:05:31 -0500

    Scaffolded the inventory tests

---
+++ UnitTests/InventoryRepositoryTests.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Models;
+using Xunit;
+using DL;
+
+
+
+namespace UnitTests
+{
+    class InventoryRepositoryTests
+    {
+        private readonly DbContextOptions<DBContext> _options;
+        public InventoryRepositoryTests()
+        {
+            _options = new DbContextOptionsBuilder<DBContext>().UseSqlite("Filename = Test.db").Options;
+            this.Seed();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        private void Seed()
+        {
+            using (var context = new DBContext(_options))
+            {
+                context.Database.EnsureDeleted();
+                context.Database.EnsureCreated();
+            }
+        }
+    }
+}
