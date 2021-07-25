11d1bddd917b8c4d2cf946614ae891bcafc5eff2
Caleb-Huss <caleb.huss97@gmail.com>
2021-07-25 17:01:53 -0500

    Started work on visuals

---
+++ WebUI/Models/StoreFrontVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebUI.Models
+{
+    public class StoreFrontVM
+    {
+        public StoreFrontVM()
+        {
+
+        }
+
+        public int StoreNumber { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+    }
+}
